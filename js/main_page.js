c8146cea72c602d99f530c724d522af28090efea
PaulBBBBB <chanpaul1234572@gmail.com>
2018-09-25 19:35:58 +0800

    Fine turn

--- js/main_page.js
+++ js/main_page.js
@@ -1,6 +1,12 @@
 jQuery(document).ready(function() {
-    $("#breadcrumbs").rcrumbs();
-    $( "#target" ).click(function() {
-        alert( "Handler for .click() called." );
+    $("#breadcrumbs").rcrumbs({nbUncollapsableCrumbs: 3});
+    $( "#p1" ).click(function() {
+        let x = $("#product1").text();
+        //console.log(x);
+        $(".drop .dropdown .dropdown-content a").last().before("<a id='item1'>item1</a>");
       });
+    let row = $(".table li").length;
+    let output_messgae = "<p class='clear'>Total: " + row + " items</p>"
+    $(".table").append(output_messgae);
+
 });
